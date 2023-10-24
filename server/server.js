d504acac1a11ce34013d46b86fa31784bb7bee03
Davi Fernandes <davifncosta@gmail.com>
2023-10-24 20:14:11 -0300

    feat: add morgan package

--- server/server.js
+++ server/server.js
@@ -1,14 +1,29 @@
 require("dotenv").config();
-
 const express = require("express");
-
 const app = express();
+const morgan = require('morgan')
 
-app.get("/getRestaurant", (req, res) => {
-  res.json({
+app.use(morgan("dev"))
+
+// Get all restaurants
+app.get("/api/v1/restaurantes", (req, res,) => {
+  console.log("yes")
+  res.status(200).json({
     status: "success",
-    restaurant: "macdonalds",
+    data: {
+      restaurant: ["macdonalds", "açai do monstro"],
+    },
   });
+});
+
+// Get 1 restaurant
+app.get("/api/v1/restaurantes/:id", (req, res) => {
+  console.log(req.params.id);
+});
+
+// Create a new restaurant
+app.post("/api/v1/restaurantes/:id", (req, res) => {
+  console.log(req)
 });
 
 const port = process.env.PORT || 3001;
