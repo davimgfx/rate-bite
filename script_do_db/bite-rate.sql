4688945c44837692b7a7409f46e732a02e6bd2af
Davi Fernandes <118557337+davimgfx@users.noreply.github.com>
2023-10-25 14:13:22 -0300

    Update bite-rate.sql

--- script_do_db/bite-rate.sql
+++ script_do_db/bite-rate.sql
@@ -87,10 +87,6 @@ VALUES
  
 -- Atendimento
 
--- Da erro porque não está entre 0 e 5
--- INSERT INTO atendimento (id_cliente, id_restaurante, avaliacao, dia)
--- VALUES (2, 1, 6, CURRENT_DATE)
-
 INSERT INTO atendimento (id_cliente, id_restaurante, avaliacao, dia)
 VALUES (2, 1, 4, CURRENT_DATE)
 
@@ -113,3 +109,32 @@ VALUES('hamburguer')
 SELECT * FROM atendimento
 
 -- Prato
+
+INSERT INTO prato (id_restaurante, id_cliente, id_categoria, nome, valor, avaliacao, dia, comentario)
+VALUES (1, 1, 3, 'Big Mac', 12.2, 3, CURRENT_DATE, 'Achei muita salgado' )
+
+INSERT INTO prato (id_restaurante, id_cliente, id_categoria, nome, valor, avaliacao, dia, comentario)
+VALUES (2, 1, 1, 'Pizza calabresa pequena', 40.2, 3, CURRENT_DATE, 'Achei muita massa, e pedi sem cebola porém veio com' )
+
+INSERT INTO prato (id_restaurante, id_cliente, id_categoria, nome, valor, avaliacao, dia, comentario)
+VALUES (3, 2, 2, 'Açai com granola', 20.5, 5, CURRENT_DATE, 'perfeito!' )
+
+-- Criando view para facilitar a visualização das tabelas juntas
+
+CREATE pratos_com_informacoes AS
+SELECT
+	prato.id_prato AS id_prato,
+    prato.nome AS nome_prato,
+    restaurante.nome AS nome_restaurante,
+    categoria.nome AS nome_categoria,
+    cliente.nome AS nome_cliente,
+    cliente.idade AS idade_cliente,
+    prato.avaliacao AS avaliacao_cliente
+FROM prato
+INNER JOIN restaurante ON prato.id_restaurante = restaurante.ID_restaurante
+INNER JOIN categoria ON prato.id_categoria = categoria.ID_categoria
+INNER JOIN cliente ON prato.id_cliente = cliente.ID_cliente;
+		
+DROP VIEW pratos_com_informacoes;
+	
+SELECT * FROM pratos_com_informacoes ORDER BY id_prato ASC;
